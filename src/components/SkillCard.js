1c5600e206219a42f28ab5b89b87132507574ecf
Arpit Gupta <arpit.8.a@gmail.com>
2020-12-22 22:09:02 +0530

    ui fix

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -8,6 +8,7 @@ import {
   CardSubtitle,
   Button,
   Badge,
+  Row,
 } from 'reactstrap';
 
 const SkillCard = (props) => {
@@ -18,33 +19,35 @@ const SkillCard = (props) => {
           <CardTitle tag="h1" className="ml-2">
             Skills
           </CardTitle>
-          <Badge color="primary" pill className="skill-badge m-2">
-            HTML5
-          </Badge>
-          <Badge color="primary" pill className="skill-badge m-2">
-            CSS3
-          </Badge>
-          <Badge color="primary" pill className="skill-badge m-2">
-            JavaScript
-          </Badge>
-          <Badge color="primary" pill className="skill-badge m-2">
-            C++
-          </Badge>
-          <Badge color="primary" pill className="skill-badge m-2">
-            Python
-          </Badge>
-          <Badge color="primary" pill className="skill-badge m-2">
-            ReactJS
-          </Badge>
-          <Badge color="primary" pill className="skill-badge m-2">
-            Git
-          </Badge>
-          <Badge color="primary" pill className="skill-badge m-2">
-            GitHub
-          </Badge>
-          <Badge color="primary" pill className="skill-badge m-2">
-            UI Design
-          </Badge>
+          <div className="my-4">
+            <Badge color="primary" pill className="skill-badge m-2">
+              HTML5
+            </Badge>
+            <Badge color="primary" pill className="skill-badge m-2">
+              CSS3
+            </Badge>
+            <Badge color="primary" pill className="skill-badge m-2">
+              JavaScript
+            </Badge>
+            <Badge color="primary" pill className="skill-badge m-2">
+              C++
+            </Badge>
+            <Badge color="primary" pill className="skill-badge m-2">
+              Python
+            </Badge>
+            <Badge color="primary" pill className="skill-badge m-2">
+              ReactJS
+            </Badge>
+            <Badge color="primary" pill className="skill-badge m-2">
+              Git
+            </Badge>
+            <Badge color="primary" pill className="skill-badge m-2">
+              GitHub
+            </Badge>
+            <Badge color="primary" pill className="skill-badge m-2">
+              UI Design
+            </Badge>
+          </div>
         </CardBody>
       </Card>
     </div>
